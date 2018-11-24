fdea7bf6bc5c9d0c699b02db102984025fa1b21b
viatali <208217@via.dk>
2018-11-24 18:06:50 +0100

    Light Control fix

--- SmartEveryDay/Controllers/LightController.cs
+++ SmartEveryDay/Controllers/LightController.cs
@@ -1,29 +1,57 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
 namespace SmartEveryDay.Controllers
 {
+
+   
     public class LightController : Controller
     {
+
+        private IRestClient client;
+        private IRestResponse response;
+        private IRestRequest request;
         // GET: Light
-        public ActionResult Index()
+        public ActionResult Light()
         {
             return View();
         }
 
-
-        [HttpGet]
-        public int turnOnLight()
+                
+           //method that calls the aRest cloud and controll the light ON  
+        public string TurnLightOn()
         {
+           
+            var client = new WebClient();
+            var content = client.DownloadString("https://cloud.arest.io/light_id1/digital/2/0");
 
-            //https://cloud.arest.io/wasdqe/digital/2/1
-
-            return 1;
+            return content;
         }
 
+        public string TurnLightOff()
+        {
+
+            var client = new WebClient();
+            var content = client.DownloadString("https://cloud.arest.io/light_id1/digital/2/1");
+
+            return content;
+        }
+
+
+        //private JsonResult GetLightOn()
+        //{
+
+        //    var myUrl = "https://cloud.arest.io/light_id1/digital/2/0";
+        //    client = new RestClient(myUrl) { };
+        //    response = client.Execute(request);
+        //    return Json(response.Content, JsonRequestBehavior.AllowGet);
+
+        //}
 
     }
 }
