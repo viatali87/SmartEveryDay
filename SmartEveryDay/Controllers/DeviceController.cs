8a7c7dcd5f84ee08cb632ce774700f8cf545c402
viatali <208217@via.dk>
2018-11-26 17:52:38 +0100

    Device Light and Curtains

--- SmartEveryDay/Controllers/DeviceController.cs
+++ SmartEveryDay/Controllers/DeviceController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,5 +14,41 @@ namespace SmartEveryDay.Controllers
         {
             return View();
         }
+
+        public string TurnCurtainsOpen()
+        {
+
+            var client = new WebClient();
+            var content = client.DownloadString(" https://cloud.arest.io/light_id1/digital/2/0");
+
+            return content;
+        }
+
+        public string TurnCurtainsClosed()
+        {
+
+            var client = new WebClient();
+            var content = client.DownloadString("https://cloud.arest.io/light_id1/digital/2/1");
+
+            return content;
+        }
+
+        public string TurnLightOn()
+        {
+
+            var client = new WebClient();
+            var content = client.DownloadString("https://cloud.arest.io/light_id1/digital/2/0");
+            return content;
+        }
+
+        public string TurnLightOff()
+        {
+
+            var client = new WebClient();
+            var content = client.DownloadString("https://cloud.arest.io/light_id1/digital/2/1");
+
+            return content;
+        }
+
     }
 }
