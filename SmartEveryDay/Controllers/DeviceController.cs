ee5cfcd231e9de05048fb87f296897a95a88b332
viatali <208217@via.dk>
2018-11-30 14:46:14 +0100

    curtains cloud

--- SmartEveryDay/Controllers/DeviceController.cs
+++ SmartEveryDay/Controllers/DeviceController.cs
@@ -178,7 +178,7 @@ namespace SmartEveryDay.Controllers
 
 
             var client = new WebClient();
-            var content = client.DownloadString("https://cloud.arest.io/blinds/up");
+            var content = client.DownloadString(" https://cloud.arest.io/blinds/up");
 
             return content;
         }
@@ -187,7 +187,7 @@ namespace SmartEveryDay.Controllers
         {
 
             var client = new WebClient();
-            var content = client.DownloadString("https://cloud.arest.io/blinds/down");
+            var content = client.DownloadString(" https://cloud.arest.io/blinds/down");
 
             return content;
         }
