22e154544784735300cb3d9b54b29dc49dec5b19
viatali <208217@via.dk>
2018-12-08 17:02:25 +0100

    Unit Test added

---
+++ UnitTestSmartEveryDay/CreateUserUnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestSmartEveryDay
+{
+    [TestClass]
+    public class CreateUserUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
